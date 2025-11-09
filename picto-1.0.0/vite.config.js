fcf363eecb9e0e61cab7c0f3666259feacccd143
Nazrul Islam <nazrul01865@gmail.com>
2025-11-09 23:26:06 +0600

    a

--- picto-1.0.0/vite.config.js
+++ picto-1.0.0/vite.config.js
@@ -1,23 +1,42 @@
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+//   return {
+//     base: `/${env.VITE_REPO_NAME}/`,
+//     plugins: [react(), tailwindcss()],
+//     server: {
+//       open: true,
+//       host: false, // Allow access from network devices
+//       proxy: {
+//         "/api": {
+//           target: "http://localhost:5000",
+//           changeOrigin: true,
+//           secure: true,
+//         },
+//       },
+//     },
+//   };
+// });
+
+// vite.config.js
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  const base = env.VITE_REPO_NAME ? `/${env.VITE_REPO_NAME}/` : "/"; // <- important
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base,
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
-      host: false, // Allow access from network devices
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
-      },
+      host: false,
+      proxy: { "/api": { target: "http://localhost:5000", changeOrigin: true, secure: true } },
     },
   };
 });
