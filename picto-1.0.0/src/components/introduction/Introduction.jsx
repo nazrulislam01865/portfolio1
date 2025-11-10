c0470aaccc65190cdd420ba5aaa2f5cf7675c8b9
Nazrul Islam <nazrul01865@gmail.com>
2025-11-10 23:02:24 +0600

    updated1

--- picto-1.0.0/src/components/introduction/Introduction.jsx
+++ picto-1.0.0/src/components/introduction/Introduction.jsx
@@ -1,4 +1,4 @@
-import person from "../../assets/images/person.png";
+import person from "../../assets/images/nazrul.webp";
 import "./introduction.css";
 import InformationSummary from "./InformationSummary";
 
--- picto-1.0.0/src/components/profile/Profile.jsx
+++ picto-1.0.0/src/components/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import person from "../../assets/images/person2.png";
+import person from "../../assets/images/nazrul.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import SocialMedia from "../common/socialMedia/SocialMedia";
